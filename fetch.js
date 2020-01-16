e70217409ecc45267452d53824caef9a3c13b49d
Kovchin Pavel <kovchin@mail.ru>
2020-01-16 14:46:14 +0700

    16.01.2020

--- fetch.js
+++ fetch.js
@@ -1,1 +1,31 @@
-console.log('I am working');
+//Входящие данные
+const body = {
+    a: 38,
+    b: 39
+}
+
+const requestURL = './req.php';
+
+//Функция генерирующая fetch запрос
+function sendRequest(method, url, body = null) {
+    //Формируем заголовок запроса (можно и без него я в php просто парсю полезную нагрузку)
+    const headers = {
+        'Content-Type': 'application/json',
+    }
+    //В выводе функции вызываем fetch запрос с подготовленными данными
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(body),
+        headers: headers
+        //Так как результатом fetch возвращается promise, то стрелочной функцией разбираем ответ как json (можно получать объекты)
+    }).then(response => {
+        return response.json();
+    })
+};
+
+//Вызов функции
+sendRequest('post', requestURL, body)
+    //Здесь ответ в случае успеха
+    .then(data => console.log(data))
+    //Здесь ответ в случае неудачи
+    .catch(err => console.error(err))
--- req.php
+++ req.php
@@ -1,4 +1,19 @@
 <?php
 
-echo 'Массив входных параметров post <br>';
-echo $_POST;
+//Ловим полезную нагрузку в запросе
+$postData = file_get_contents('php://input');
+$data = json_decode($postData, true);
+
+//Развлекаемся с полученными данными
+$a = $data['a'];
+$b = $data['b'];
+
+//Формируем ответ
+$res = array(
+    'a' => $a,
+    'b' => $b,
+    'c' => $a + $b
+);
+
+//Отправляем ответ
+print_r(json_encode($res));
